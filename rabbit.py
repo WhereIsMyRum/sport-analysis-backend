1dc6ac9491997acdad063eabbc2f6bf0ad83f648
WhereIsMyRum <polcik.piotr@gmail.com>
2019-09-24 21:29:23 +0200

    Initial rabbitmq setup

---
+++ rabbit.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='bucketevents',
+                         exchange_type='fanout')
+
+result = channel.queue_declare("",exclusive=False)
+queue_name = result.method.queue
+
+channel.queue_bind(exchange='bucketevents',
+                   queue=queue_name)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(" [x] %r" % body)
+
+channel.basic_consume(on_message_callback=callback, queue=queue_name)
+
+channel.start_consuming()
